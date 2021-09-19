a83bdf86b828515c12c6eba553db2fc0b9a638bd
Praveen Kumar <praveenscience@gmail.com>
2021-09-19 13:23:30 +0100

    Imported Jimp and read the image file.

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const fs = require("fs");
+const Jimp = require("jimp");
 const Students = fs.readFileSync("./students.csv");
 const StudentsArr = Students.toString().split(`
 `);
 StudentsArr.forEach(stuName => {
   console.log(`Printing Certificate for ${stuName}`);
 });
+
+Jimp.read("./ticket.png");
