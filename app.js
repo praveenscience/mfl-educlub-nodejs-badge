bb875799b6b2b98bc5aa009bd8c0b8ee908d151c
Praveen Kumar <praveenscience@gmail.com>
2021-09-19 13:28:12 +0100

    Write text on the loaded image.

--- app.js
+++ app.js
@@ -8,7 +8,13 @@ StudentsArr.forEach(stuName => {
 });
 
 let loadedImage;
-Jimp.read("./ticket.png").then(image => {
-  loadedImage = image;
-  return Jimp.loadFont(Jimp.FONT_SANS_64_BLACK);
-});
+Jimp.read("./ticket.png")
+  .then(image => {
+    loadedImage = image;
+    return Jimp.loadFont(Jimp.FONT_SANS_64_BLACK);
+  })
+  .then(font => {
+    loadedImage
+      .print(font, 15, 15, "Here Lies your Name!")
+      .write("PrintedTicket.png");
+  });
