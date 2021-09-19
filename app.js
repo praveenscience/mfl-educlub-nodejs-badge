72608aa7e6e6dd63fef7e32edd356697e5a6684d
Praveen Kumar <praveenscience@gmail.com>
2021-09-19 13:50:12 +0100

    Fixed the bugs and added the loop.

--- app.js
+++ app.js
@@ -1,31 +1,41 @@
 const fs = require("fs");
 const Jimp = require("jimp");
 const Students = fs.readFileSync("./students.csv");
+
 const StudentsArr = Students.toString().split(`
 `);
-StudentsArr.forEach(stuName => {
-  console.log(`Printing Certificate for ${stuName}`);
-});
 
 let loadedImage;
-Jimp.read("./ticket.png")
-  .then(image => {
-    loadedImage = image;
-    return Jimp.loadFont(Jimp.FONT_SANS_64_BLACK);
-  })
-  .then(font => {
-    loadedImage
-      .print(
-        font,
-        450,
-        290,
-        {
-          text: "Hello!",
-          alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
-          alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE
-        },
-        1080,
-        50
-      )
-      .write("PrintedTicket.png");
+
+const PrintTicket = (File, Text) => {
+  return Jimp.read("ticket.png")
+    .then(image => {
+      loadedImage = image;
+      return Jimp.loadFont(Jimp.FONT_SANS_64_BLACK);
+    })
+    .then(font => {
+      loadedImage
+        .print(
+          font,
+          365,
+          290,
+          {
+            text: Text,
+            alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+            alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE
+          },
+          1000,
+          50
+        )
+        .write(File);
+    });
+};
+
+const main = async () => {
+  StudentsArr.forEach(stuName => {
+    console.log(`Priting Certificate for ${stuName}`);
+    await PrintTicket(stuName.trim().replace(/\s/g, "-") + ".png", stuName);
   });
+};
+
+main();
