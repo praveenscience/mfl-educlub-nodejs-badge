cec723171e61eb36c404c9ce78fc3e75a012b75f
Praveen Kumar <praveenscience@gmail.com>
2021-09-19 13:20:17 +0100

    Loop through Students Array and print out something.

--- app.js
+++ app.js
@@ -2,3 +2,6 @@ const fs = require("fs");
 const Students = fs.readFileSync("./students.csv");
 const StudentsArr = Students.toString().split(`
 `);
+StudentsArr.forEach(stuName => {
+  console.log(`Printing Certificate for ${stuName}`);
+});
